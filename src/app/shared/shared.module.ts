b96f221c7d660dbe9c655420ab044ad9f2a5af23
moisesarrona <arronamoisesar@outlook.com>
2022-10-03 22:59:31 -0500

    Component example deleted

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,7 +5,6 @@ import { SharedRoutingModule } from './shared-routing.module';
 import { MinitaskTemplateComponent } from './minitask-template/minitask-template.component';
 import { HeaderComponent } from './minitask-template/header/header.component';
 import { SidebarComponent } from './minitask-template/sidebar/sidebar.component';
-import { ExampleComponent } from './example/example.component';
 
 
 @NgModule({
@@ -13,7 +12,6 @@ import { ExampleComponent } from './example/example.component';
     MinitaskTemplateComponent,
     HeaderComponent,
     SidebarComponent,
-    ExampleComponent
   ],
   imports: [
     CommonModule,

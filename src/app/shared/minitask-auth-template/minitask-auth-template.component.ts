2cf42496e1ffce58dc5ba6c7ad231dd596c8f1fb
moisesarrona <arronamoisesar@outlook.com>
2022-10-26 23:41:04 -0500

    Add dark mode

--- src/app/shared/minitask-auth-template/minitask-auth-template.component.ts
+++ src/app/shared/minitask-auth-template/minitask-auth-template.component.ts
@@ -9,12 +9,18 @@ export class MinitaskAuthTemplateComponent implements OnInit {
   public authShow: boolean = true
 
   constructor() { }
+  public themeDark: boolean = false;
 
   ngOnInit(): void {
+    this.themeDark = localStorage.getItem('landing-theme')? true : false
   }
 
   public showAuthEvent = ():void => {
     this.authShow = !this.authShow
   }
 
+  public changeTheme = (data: boolean): any => {
+    data? localStorage.setItem('landing-theme', 'dark') : localStorage.removeItem('landing-theme')
+    this.themeDark = data
+  }
 }

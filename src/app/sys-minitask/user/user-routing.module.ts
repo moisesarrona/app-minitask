85a24a01ed6a068a36a06826fcea6e414402b27f
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-02-15 23:14:00 -0600

    Add Routing to profile-edith

--- src/app/sys-minitask/user/user-routing.module.ts
+++ src/app/sys-minitask/user/user-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileEditComponent } from './components/profile-edit/profile-edit.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
   {
     path: 'perfil',
     component: ProfileComponent
+  },
+  {
+    path: 'perfil-update',
+    component: ProfileEditComponent
   },
   {
     path: '**',

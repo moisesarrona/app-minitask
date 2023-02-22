50414302b2a33b3b579f77ac60c4126773e65a48
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-02-21 23:35:42 -0600

    Delete imports in ModuleUser

--- src/app/sys-minitask/user/user.module.ts
+++ src/app/sys-minitask/user/user.module.ts
@@ -5,7 +5,6 @@ import { UserRoutingModule } from './user-routing.module';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ProfileEditComponent } from './components/profile-edit/profile-edit.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
